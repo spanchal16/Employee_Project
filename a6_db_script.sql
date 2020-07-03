504dec47cbd703343de1fbfe6a25105f3f4b7616
Nawaphan Chayopathum <nw814986@dal.ca>
2020-07-03 18:24:15 -0300

    Update a6_db_script.sql

--- a6_db_script.sql
+++ a6_db_script.sql
@@ -1,9 +1,21 @@
+CREATE TABLE IF NOT EXISTS cloud.parts (
+partId int NOT NULL,
+partName VARCHAR(10) NOT NULL,
+qty int NOT NULL,
+PRIMARY KEY (partId));
+
+INSERT INTO cloud.parts VALUES (1, 'part1', 100);
+INSERT INTO cloud.parts VALUES (2, 'part2', 200);
+INSERT INTO cloud.parts VALUES (3, 'part3', 300);
+
+select * from cloud.parts;
+
 CREATE TABLE IF NOT EXISTS cloud.jobs (
 jobName VARCHAR(10) NOT NULL,
-partID int NOT NULL,
+partId int NOT NULL,
 qty int NOT NULL,
-PRIMARY KEY (jobName, partID),
-FOREIGN KEY(partID) REFERENCES parts(partID));
+PRIMARY KEY (jobName, partId),
+FOREIGN KEY(partId) REFERENCES parts(partId));
 
 INSERT INTO cloud.jobs VALUES ('job1', 1, 1);
 INSERT INTO cloud.jobs VALUES ('job1', 2, 1);
@@ -14,29 +26,17 @@ INSERT INTO cloud.jobs VALUES ('job3', 3, 3);
 
 select * from cloud.jobs;
 
-CREATE TABLE IF NOT EXISTS cloud.parts (
-partID int NOT NULL,
-partName VARCHAR(10) NOT NULL,
-qty int NOT NULL,
-PRIMARY KEY (partID));
-
-INSERT INTO cloud.parts VALUES (1, 'part1', 100);
-INSERT INTO cloud.parts VALUES (2, 'part2', 200);
-INSERT INTO cloud.parts VALUES (3, 'part3', 300);
-
-select * from cloud.parts;
-
-CREATE TABLE IF NOT EXISTS cloud.partOrders (
+CREATE TABLE IF NOT EXISTS cloud.partOrdersX (
 jobName VARCHAR(10) NOT NULL,
-partID int NOT NULL,
-userID int NOT NULL,
+partId int NOT NULL,
+userId int NOT NULL,
 qty int NOT NULL,
-PRIMARY KEY (jobName, partID, userID),
+PRIMARY KEY (jobName, partId, userId),
 FOREIGN KEY(jobName) REFERENCES jobs(jobName),
-FOREIGN KEY(partID) REFERENCES parts(partID));
+FOREIGN KEY(partId) REFERENCES parts(partId));
 
-INSERT INTO cloud.partOrders VALUES ('job1', 1, '101', 1);
-INSERT INTO cloud.partOrders VALUES ('job2', 2, '102', 2);
-INSERT INTO cloud.partOrders VALUES ('job3', 3, '103', 3);
+INSERT INTO cloud.partOrdersX VALUES ('job1', 1, '101', 1);
+INSERT INTO cloud.partOrdersX VALUES ('job2', 2, '102', 2);
+INSERT INTO cloud.partOrdersX VALUES ('job3', 3, '103', 3);
 
-select * from cloud.partOrders;
+select * from cloud.partOrdersX;
