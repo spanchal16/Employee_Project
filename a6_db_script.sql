9822e3526555da5aa3a962d590d995105e42ca40
Nawaphan Chayopathum <nw814986@dal.ca>
2020-07-03 14:37:16 -0300

    Add database script

---
+++ a6_db_script.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS cloud.jobs (
+jobName VARCHAR(10) NOT NULL,
+partID int NOT NULL,
+qty int NOT NULL,
+PRIMARY KEY (jobName, partID),
+FOREIGN KEY(partID) REFERENCES parts(partID));
+
+INSERT INTO cloud.jobs VALUES ('job1', 1, 1);
+INSERT INTO cloud.jobs VALUES ('job1', 2, 1);
+INSERT INTO cloud.jobs VALUES ('job2', 2, 2);
+INSERT INTO cloud.jobs VALUES ('job2', 3, 2);
+INSERT INTO cloud.jobs VALUES ('job3', 1, 3);
+INSERT INTO cloud.jobs VALUES ('job3', 3, 3);
+
+select * from cloud.jobs;
+
+CREATE TABLE IF NOT EXISTS cloud.parts (
+partID int NOT NULL,
+partName VARCHAR(10) NOT NULL,
+qty int NOT NULL,
+PRIMARY KEY (partID));
+
+INSERT INTO cloud.parts VALUES (1, 'part1', 100);
+INSERT INTO cloud.parts VALUES (2, 'part2', 200);
+INSERT INTO cloud.parts VALUES (3, 'part3', 300);
+
+select * from cloud.parts;
+
+CREATE TABLE IF NOT EXISTS cloud.partOrders (
+jobName VARCHAR(10) NOT NULL,
+partID int NOT NULL,
+userID int NOT NULL,
+qty int NOT NULL,
+PRIMARY KEY (jobName, partID, userID),
+FOREIGN KEY(jobName) REFERENCES jobs(jobName),
+FOREIGN KEY(partID) REFERENCES parts(partID));
+
+INSERT INTO cloud.partOrders VALUES ('job1', 1, '101', 1);
+INSERT INTO cloud.partOrders VALUES ('job2', 2, '102', 2);
+INSERT INTO cloud.partOrders VALUES ('job3', 3, '103', 3);
+
+select * from cloud.partOrders;
